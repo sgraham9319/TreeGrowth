4c26cb6363203323f5edcdf85c3363f382de1abf
sgraham9319 <sgraham3@uw.edu>
2021-04-12 15:10:45 +0200

    Created script for reinstalling ForestPlot

---
+++ Scripts/Installing_ForestPlot.R
@@ -0,0 +1,5 @@
+# If need to reinstall ForestPlot to get new changes, uninstall in Packages
+# window of RStudio then run this file
+
+library(devtools)
+devtools::install_github("sgraham9319/ForestPlot")
