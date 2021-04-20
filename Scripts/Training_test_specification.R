fd73736f78743feeca851ce963232a9dd920957b
sgraham9319 <sgraham3@uw.edu>
2021-04-20 10:12:52 +0200

    Specified random training and test sets

--- Scripts/Training_test_specification.R
+++ Scripts/Training_test_specification.R
@@ -3,6 +3,10 @@ library(dplyr)
 # Load neighborhood data
 all_nbhds <- read.csv("Data/Output_data/neighborhoods.csv", stringsAsFactors = F)
 
+#=============================================
+# Specifying spatially separated training sets
+#=============================================
+
 # Assign quadrants of each stand to a test set
 set.seed(500)
 stand <- rep(unique(all_nbhds$stand_id), each = 4)
@@ -77,4 +81,46 @@ for(set in 1:4){
   write.csv(training, paste("Data/Output_data/training", set, ".csv", sep = ""),
             row.names = F)
   
+}
+
+#================================
+# Specifying random training sets
+#================================
+
+# Set seed to ensure consistent test set specification
+set.seed(100)
+
+# Assign each tree to a test set
+test_assign <- all_nbhds %>%
+  group_by(tree_id) %>%
+  summarize(stand_id = stand_id[1],
+            species = species[1]) %>%
+  group_by(stand_id) %>%
+  mutate(test_set = sample(rep(1:4, length.out = n()), n())) %>%
+  ungroup()
+
+# Confirm equal number of trees in each test set per stand
+table(test_assign$stand_id, test_assign$test_set)
+
+# Confirm relatively equal test set distribution per species
+table(test_assign$species, test_assign$test_set)
+
+# Join to neighborhood data
+rand_nbhds <- all_nbhds %>%
+  left_join(test_assign %>% select(tree_id, test_set), by = "tree_id")
+
+# Write random training and test sets to .csv
+for(set in 1:4){
+  
+  # Isolate this training and test set
+  test <- rand_nbhds %>%
+    filter(test_set == set)
+  training <- rand_nbhds %>%
+    filter(test_set != set)
+  
+  # Write to .csv
+  write.csv(test, paste("Data/Output_data/rand_test", set, ".csv", sep = ""),
+            row.names = F)
+  write.csv(training, paste("Data/Output_data/rand_training", set, ".csv", sep = ""),
+            row.names = F)
 }
