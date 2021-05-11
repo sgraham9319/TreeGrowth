38e0af84ac9d97cf36caab94197dd44b779ae4f6
sgraham9319 <sgraham3@uw.edu>
2021-05-11 10:17:08 +0200

    Created fit comparison tables

--- Scripts/Consistency_among_training_sets.R
+++ Scripts/Consistency_among_training_sets.R
@@ -27,5 +27,28 @@ lkhd_table_rand <- lkhd_model_select(training_type = "random")
 #write.csv(lkhd_table_rand, "Figures/lkhd_model_selection_rand_train.csv",
 #          row.names = F)
 
-a <- quantify_lkhd_fit(train_type = "regular")
-b <- quantify_lkhd_fit(train_type = "random")
+lkhd_r2 <- quantify_lkhd_fit(train_type = "regular")
+lkhd_r2_rand <- quantify_lkhd_fit(train_type = "random")
+
+# Load regularized regression r2
+rr_r2 <- read.csv("Data/Figure_data/RR_r2.csv")
+rr_r2_rand <- read.csv("Data/Figure_data/RR_r2_rand.csv")
+
+# Combine into single r2 table
+r2_table <- lkhd_r2 %>%
+  left_join(rr_r2, by = c("focal_sps" = "species",
+                          "training_set" = "training")) %>%
+  rename(train_rr = train_r2, test_rr = test_r2) %>%
+  select(focal_sps, training_set, sample_size, train_lkhd, train_rr, test_lkhd,
+         test_rr)
+r2_rand_table <- lkhd_r2_rand %>%
+  left_join(rr_r2_rand, by = c("focal_sps" = "species",
+                               "training_set" = "training")) %>%
+  rename(train_rr = train_r2, test_rr = test_r2) %>%
+  select(focal_sps, training_set, sample_size, train_lkhd, train_rr, test_lkhd,
+         test_rr)
+
+# Save r2 tables
+#write.csv(r2_table, "Figures/train_test_fit.csv", row.names = F)
+#write.csv(r2_rand_table, "Figures/train_test_fit_rand.csv", row.names = F)
+
