d9bd5193162aeb4b94d46b61d0a93e0d4da69c1a
sgraham9319 <sgraham3@uw.edu>
2021-06-09 10:43:27 +0200

    Created new model fit table including cv

--- Scripts/Consistency_among_training_sets.R
+++ Scripts/Consistency_among_training_sets.R
@@ -19,35 +19,46 @@ comparison$comp_plot
 # View all results
 View(comparison$best_models)
 
-# Create likelihood model selection table
-lkhd_table_rand <- lkhd_model_select(training_type = "random")
+# Create likelihood AIC model selection table
+lkhd_table_rand <- lkhd_model_select()
+
+# Create likelihood cv model selection table
+lkhd_table_cv <- lkhd_model_select(method = "cv", num_train_sets = 2)
 
 # Save model selection tables
 write.csv(lkhd_table_rand, "Figures/lkhd_model_selection_rand_train.csv",
           row.names = F)
+write.csv(lkhd_table_cv, "Figures/lkhd_model_selection_cv.csv",
+          row.names = F)
 
 # Calculate likelihood r2 values
-lkhd_r2_rand <- quantify_lkhd_fit(method = "AIC")
+lkhd_r2_rand <- quantify_lkhd_fit()
+lkhd_r2_cv <- quantify_lkhd_fit(method = "cv", num_train_sets = 2)
 
 # Load regularized regression r2
 rr_r2_rand <- read.csv("Data/Figure_data/RR_r2_rand.csv")
 
 # Combine into single r2 table
-r2_rand_table <- lkhd_r2_rand %>%
+r2_table <- lkhd_r2_rand %>%
+  left_join(lkhd_r2_cv %>% select(-sample_size),
+            by = c("focal_sps", "training_set"),
+            suffix = c("_AIC", "_cv")) %>%
   left_join(rr_r2_rand, by = c("focal_sps" = "species",
                                "training_set" = "training")) %>%
   rename(train_rr = train_r2, test_rr = test_r2) %>%
-  select(focal_sps, training_set, sample_size, train_lkhd, train_rr, test_lkhd,
-         test_rr) %>%
-  mutate(train_lkhd = round(train_lkhd, 2),
+  select(focal_sps, training_set, sample_size, train_lkhd_AIC, train_lkhd_cv,
+         train_rr, test_lkhd_AIC, test_lkhd_cv, test_rr) %>%
+  mutate(train_lkhd_AIC = round(train_lkhd_AIC, 2),
+         train_lkhd_cv = round(train_lkhd_cv, 2),
          train_rr = round(train_rr, 2),
-         test_lkhd = round(test_lkhd, 2),
+         test_lkhd_AIC = round(test_lkhd_AIC, 2),
+         test_lkhd_cv = round(test_lkhd_cv, 2),
          test_rr = round(test_rr, 2))
 
-# Save r2 tables
-write.csv(r2_rand_table, "Figures/train_test_fit_rand.csv", row.names = F)
+# Save r2 table
+write.csv(r2_table, "Figures/all_train_test_fit.csv", row.names = F)
 
 # Make R2 figures
-r2_rand <- read.csv("Figures/train_test_fit_rand.csv", stringsAsFactors = F)
+r2_all <- read.csv("Figures/all_train_test_fit.csv", stringsAsFactors = F)
 r2_figure(r2_rand, "train_test_fit_rand")
 r2_figure(r2_rand, "predict_fits", cv = T)
