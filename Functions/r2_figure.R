3d05ab46e47d89acc4d4eab8e7304b9e387bc2d8
sgraham9319 <sgraham3@uw.edu>
2021-08-30 13:53:41 -0700

    Changed prediction figure format to JPEG

--- Functions/r2_figure.R
+++ Functions/r2_figure.R
@@ -40,11 +40,11 @@ r2_figure <- function(r2_table, name){
     filter(train_test == "test")
   
   # Initiate plot saving
-  png(filename = paste("Figures/", name, ".png", sep = ""),
+  jpeg(filename = paste("Figures/", name, ".jpg", sep = ""),
       type = "cairo",
-      units = "in",
-      width = 4,
-      height = 5,
+      units = "px",
+      width = 2500,
+      height = 3125,
       pointsize = 12,
       res = 500)
   
@@ -55,35 +55,39 @@ r2_figure <- function(r2_table, name){
       mgp = c(3, 0.8, 0))
   
   # Create training plot
-  plot(training$x, training$mean, pch = 19, cex = 0.7,
+  plot(training$x, training$mean, pch = 19, cex = 1,
        col = training$cols, las = 1,
        ylim = c(min(min(training$min), 0), 0.8),
        yaxt = "n", xaxt = "n")
   axis(side = 2, at = seq(0, 0.8, 0.4), labels = seq(0, 0.8, 0.4),
-       tck = -0.02, las = 1, cex.axis = 0.9, mgp = c(3, 0.5, 0))
-  arrows(training$x, training$min, training$x, 
-         training$max, code = 3, angle = 90, length = 0, col = training$cols)
-  abline(h = 0, lty = "dashed")
-  text(x = 0.82, y = 0.78, label = "A")
+       tck = -0.02, las = 1, cex.axis = 1.1, mgp = c(3, 0.5, 0),
+       hadj = 1)
+  arrows(training$x, training$min, training$x, training$max,
+         code = 3, angle = 90, length = 0, lwd = 1.5, col = training$cols)
+  abline(h = 0, lty = "dashed", lwd = 1)
+  text(x = 0.9, y = 0.76, label = "(a)", cex = 1.2)
   legend(x = "topright", legend = c("AIC lkhd", "CV lkhd", "RR"),
-         col = c("#009292", "#b66dff", "#db6d00"), lty = 1, bty = "n", cex = 0.8)
+         col = c("#009292", "#b66dff", "#db6d00"), lty = 1, bty = "n",
+         cex = 1, lwd = 2)
   
   # Create test plot
-  plot(test$x, test$mean, pch = 19, cex = 0.7,
+  plot(test$x, test$mean, pch = 19, cex = 1,
        col = test$cols, las = 1,
        ylim = c(min(min(test$min), 0), 0.9),
        yaxt = "n", xaxt = "n")
   axis(side = 2, at = seq(0, 0.8, 0.4), labels = seq(0, 0.8, 0.4),
-       tck = -0.02, las = 1, cex.axis = 0.9, mgp = c(3, 0.5, 0))
-  arrows(test$x, test$min, test$x, 
-         test$max, code = 3, angle = 90, length = 0, col = test$cols)
-  abline(h = 0, lty = "dashed")
-  text(x = 0.82, y = 0.86, label = "B")
-  mtext(text = "Focal species", side = 1, line = 1.5)
-  mtext(text = "Coefficient of determination", side = 2, line = 2, outer = T)
+       tck = -0.02, las = 1, cex.axis = 1.1, mgp = c(3, 0.5, 0),
+       hadj = 1)
+  arrows(test$x, test$min, test$x, test$max,
+         code = 3, angle = 90, length = 0, lwd = 1.5, col = test$cols)
+  abline(h = 0, lty = "dashed", lwd = 1)
+  text(x = 0.9, y = 0.85, label = "(b)", cex = 1.2)
+  mtext(text = "Focal species", side = 1, line = 2, cex = 1.5)
+  mtext(text = "Coefficient of determination", side = 2, line = 2, outer = T,
+        cex = 1.5)
   axis(side = 1, at = 1:6,
        labels = c("ABAM", "CANO", "PSME", "THPL", "TSHE", "TSME"),
-       cex.axis = 0.8, tck = -0.02, padj = -1)
+       cex.axis = 0.9, tck = -0.02, padj = -1)
   
   # Save plot
   dev.off()
