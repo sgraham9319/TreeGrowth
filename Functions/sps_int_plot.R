e97cfb7eaf16c0816e5fe0a9d547af4a72605a04
sgraham9319 <sgraham3@uw.edu>
2021-08-30 10:12:27 -0700

    Changed species interaction figures to JPEG

--- Functions/sps_int_plot.R
+++ Functions/sps_int_plot.R
@@ -145,25 +145,25 @@ sps_int_plot <- function(focal_sps, train_type){
   
   # Set plot saving parameters
   if(train_type == "regular"){
-    png(filename = paste("Figures/Sps_int", focal_sps, ".png", sep = ""),
+    jpeg(filename = paste("Figures/Sps_int", focal_sps, ".jpg", sep = ""),
         type = "cairo",
-        units = "in", 
-        width = 5, 
-        height = 4, 
+        units = "px", 
+        width = 1250, 
+        height = 1000, 
         pointsize = 12, 
         res = 96)
   } else if(train_type == "random"){
-    png(filename = paste("Figures/Sps_int_rand", focal_sps, ".png", sep = ""),
+    jpeg(filename = paste("Figures/Sps_int_rand", focal_sps, ".jpg", sep = ""),
         type = "cairo",
-        units = "in", 
-        width = 5, 
-        height = 4, 
+        units = "px", 
+        width = 1250, 
+        height = 1000, 
         pointsize = 12, 
         res = 96)
   }
   
   # Change plot margins
-  par(mar = c(2, 2.5, 0, 2))
+  par(mar = c(2, 5, 0, 4))
   
   # Create space for legend
   layout(matrix(1:2, nrow = 2), width = c(1, 1), height = c(5, 1))
@@ -172,31 +172,33 @@ sps_int_plot <- function(focal_sps, train_type){
   plot.new()
   
   # Add rectangles
-  rect(x_left, y_bottom, x_right, y_top, col = colors[rank[0:length(all_int)]])
+  rect(x_left, y_bottom, x_right, y_top, col = colors[rank[0:length(all_int)]],
+       lwd = 2)
   
   # Reorder comps and av_ints so "other" is at the bottom
   new_comps <- c(comps[-other_index], "OTHER")
   new_av_ints <- c(av_ints[-other_index], av_ints[other_index])
   
   # Add axis labels
-  mtext(text = 1:4, side = 1, at = seq(0.125, 0.875, 0.25), line = -0.2)
-  mtext(text = "Training set", side = 1, line = 1)
-  mtext(text = new_comps, side = 2,
+  mtext(text = 1:4, side = 1, at = seq(0.125, 0.875, 0.25), line = -0.2,
+        cex = 2.5)
+  mtext(text = "Training set", side = 1, line = 1.5, cex = 2.5)
+  mtext(text = new_comps, side = 2, line = -0.8,
         at = rev(seq(rect_ht, block * length(comps) - (rect_ht + separator),
-                     block)), las = 1, cex = 0.8)
-  mtext(text = new_av_ints, side = 4,
+                     block)), las = 1, cex = 2)
+  mtext(text = new_av_ints, side = 4, line = -0.8,
         at = rev(seq(rect_ht, block * length(comps) - (rect_ht + separator),
-                     block)), las = 1, cex = 0.8)
+                     block)), las = 1, cex = 2)
   
   # Create and add legend
-  par(mar = c(1, 2.5, 0, 2))
+  par(mar = c(1, 5, 0, 4))
   plot.new()
   legend_image <- as.raster(matrix(colors, nrow = 1))
   rasterImage(legend_image, xleft = 0.25, ybottom = 0.25,
               xright = 0.75, ytop = 0.75)
   mtext(text = c(paste("Low", focal_sps, "Growth", sep = "\n"),
                  paste("High", focal_sps, "Growth", sep = "\n")),
-        side = 1, at = c(0.13, 0.87), line = -0.4, cex = 0.9)
+        side = 1, at = c(0.15, 0.85), line = -1.5, cex = 2.5)
   
   # Output plot
   dev.off()
